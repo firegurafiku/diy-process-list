47377003443297f03a6962fa2d1f34ee46308e0c
Pavel Kretov <firegurafiku@gmail.com>
2016-11-29 11:48:12 +0300

    Add assembly metadata for ProcessList project
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- ProcessList/Properties/AssemblyInfo.cs
+++ ProcessList/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DIYProcessList")]
+[assembly: AssemblyTitle("ProcessList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DIYProcessList")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("DIY Process list")]
+[assembly: AssemblyCopyright("Copyright © 2016, Pavel Kretov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
