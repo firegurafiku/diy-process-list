edf212a64e82a1cc4dad017a31dc4de478f74ff4
Pavel Kretov <firegurafiku@gmail.com>
2016-11-29 18:32:05 +0300

    Design basic MainForm and ProcessListControl
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- ProcessList/MainForm.Designer.cs
+++ ProcessList/MainForm.Designer.cs
@@ -28,53 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.listView1 = new System.Windows.Forms.ListView();
+            ProcessList.ProcessLister processLister1 = new ProcessList.ProcessLister();
+            this.uxSortKeyComboBox = new System.Windows.Forms.ComboBox();
+            this.uiDescriptionLabel = new System.Windows.Forms.Label();
+            this.uxExitButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.processListControl1 = new ProcessList.ProcessListControl();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // uxSortKeyComboBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(58, 74);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(414, 251);
-            this.listBox1.TabIndex = 0;
+            this.uxSortKeyComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uxSortKeyComboBox.FormattingEnabled = true;
+            this.uxSortKeyComboBox.Location = new System.Drawing.Point(269, 16);
+            this.uxSortKeyComboBox.Name = "uxSortKeyComboBox";
+            this.uxSortKeyComboBox.Size = new System.Drawing.Size(165, 21);
+            this.uxSortKeyComboBox.TabIndex = 2;
             // 
-            // treeView1
+            // uiDescriptionLabel
             // 
-            this.treeView1.Location = new System.Drawing.Point(69, 359);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(222, 146);
-            this.treeView1.TabIndex = 1;
+            this.uiDescriptionLabel.AutoSize = true;
+            this.uiDescriptionLabel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.uiDescriptionLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.uiDescriptionLabel.Location = new System.Drawing.Point(3, 0);
+            this.uiDescriptionLabel.Name = "uiDescriptionLabel";
+            this.uiDescriptionLabel.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel1.SetRowSpan(this.uiDescriptionLabel, 3);
+            this.uiDescriptionLabel.Size = new System.Drawing.Size(246, 49);
+            this.uiDescriptionLabel.TabIndex = 2;
+            this.uiDescriptionLabel.Text = "Below is the list of all processes we were able to obtain access to; there may be" +
+    " other projects running on the system.\r\n";
             // 
-            // listView1
+            // uxExitButton
             // 
-            this.listView1.Location = new System.Drawing.Point(360, 359);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 97);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.uxExitButton.Location = new System.Drawing.Point(3, 507);
+            this.uxExitButton.Name = "uxExitButton";
+            this.uxExitButton.Size = new System.Drawing.Size(75, 23);
+            this.uxExitButton.TabIndex = 5;
+            this.uxExitButton.Text = "E&xit";
+            this.uxExitButton.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.uiDescriptionLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.processListControl1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.uxSortKeyComboBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.uxExitButton, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(437, 533);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Order &by property";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(269, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "&Descending";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // processListControl1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.processListControl1, 2);
+            this.processListControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.processListControl1.Location = new System.Drawing.Point(3, 71);
+            this.processListControl1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 3);
+            this.processListControl1.Name = "processListControl1";
+            this.processListControl1.ProcessLister = processLister1;
+            this.processListControl1.ProcessSortDescending = false;
+            this.processListControl1.ProcessSortKey = null;
+            this.processListControl1.Size = new System.Drawing.Size(431, 430);
+            this.processListControl1.TabIndex = 4;
+            this.processListControl1.Text = "processListControl1";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 541);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.listBox1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(445, 541);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(4);
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.ListView listView1;
+        private ProcessListControl processListControl1;
+        private System.Windows.Forms.ComboBox uxSortKeyComboBox;
+        private System.Windows.Forms.Label uiDescriptionLabel;
+        private System.Windows.Forms.Button uxExitButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+
     }
 }
 
---
+++ ProcessList/ProcessListControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ProcessList
+{
+    public partial class ProcessListControl : Control
+    {
+        public ProcessListControl()
+        {
+            _listView = new ListView();
+            _listView.Parent = this;
+            _listView.Dock = DockStyle.Fill;
+            _listView.View = View.Details;
+            _listView.Columns.Add("ID");
+            _listView.Columns.Add("Process name");
+            _listView.Columns.Add("Start time");
+
+            ProcessLister = new ProcessLister();
+
+            // 
+            PopulateItems();
+        }
+
+        public PropertyInfo ProcessSortKey { get; set; }
+
+        public bool ProcessSortDescending { get; set; }
+
+        public ProcessLister ProcessLister { get; set; }
+
+        private void PopulateItems()
+        {
+            var processes = new List<Process>();
+            if (ProcessLister != null)
+                processes.AddRange(ProcessLister.ListProcesses());
+
+            _listView.BeginUpdate();
+            _listView.Items.Clear();
+            foreach (var process in processes)
+            {
+                var cellTexts = new string[] {
+                    process.Id.ToString(),
+                    process.ProcessName,
+                    process.StartTime.ToShortDateString(),
+                };
+
+                var item = new ListViewItem(cellTexts);
+                _listView.Items.Add(item);
+            }
+            
+            _listView.EndUpdate();
+
+        }
+
+        private ListView _listView;
+    }
+}
